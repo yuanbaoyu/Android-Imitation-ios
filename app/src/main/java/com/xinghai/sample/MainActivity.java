a0bb07731202db890a67827b5c19f6ec8461b2e2
yuanbaoyu <285140832@qq.com>
2016-07-26 14:03:19 +0800

    添加wheelview 及 示例

--- app/src/main/java/com/xinghai/sample/MainActivity.java
+++ app/src/main/java/com/xinghai/sample/MainActivity.java
@@ -1,25 +1,37 @@
 package com.xinghai.sample;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 
+import com.xinghai.imitation_ios.Drawables;
 import com.xinghai.imitation_ios.svprogresshud.SVProgressHUD;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn,pvBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+
+        pvBtn = (Button) findViewById(R.id.PvBtn);
     }
 
-    public void toSVProgressHudDemoActivity(View view){
+    public void toSVProgressHudDemo(View view){
         startActivity(new Intent(this,SVProgressHudDemoActivity.class));
+    }
+
+    public void toPickerViewDemo(View view){
+        startActivity(new Intent(this,PickerViewActivity.class));
     }
 
 }
---
+++ library/src/main/java/com/xinghai/imitation_ios/pickerview/lib/OnItemSelectedRunnable.java
@@ -0,0 +1,14 @@
+package com.xinghai.imitation_ios.pickerview.lib;
+
+final class OnItemSelectedRunnable implements Runnable {
+    final WheelView loopView;
+
+    OnItemSelectedRunnable(WheelView loopview) {
+        loopView = loopview;
+    }
+
+    @Override
+    public final void run() {
+        loopView.onItemSelectedListener.onItemSelected(loopView.getCurrentItem());
+    }
+}
